658318701a77365b21f3404abf8b94147bd72fdf
yjb23tech <yjb23tech@gmail.com>
2023-07-09 16:23:58 +0100

    Consistency is key

---
+++ playground_iv.py
@@ -0,0 +1,10 @@
+#python is funny in that you don't need to declare data types for vars 
+#all you need to do is start typing and assign into the variable label 
+#no special markers required 
+
+var_1 = 10 
+var_2 = 19
+
+total_sum = var_1 + var_2 
+
+print(total_sum)
